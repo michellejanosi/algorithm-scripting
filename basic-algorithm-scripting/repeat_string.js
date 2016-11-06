b83b534b6d8a905fa5d659a00c8d5687e0bae4bb
michellejanosi <michelle.janosi@gmail.com>
2016-11-06 14:02:07 -0500

    Added solution to algorithm challenge

---
+++ basic-algorithm-scripting/repeat_string.js
@@ -0,0 +1,15 @@
+/*
+Repeat a given string (first argument) num times (second argument). Return an empty
+string if num is not a positive number.
+*/
+
+function repeatStringNumTimes(str, num) {
+  // if num is negative, an empty string is returned
+  if(num < 0) {
+    return "";
+  }
+  // use the built in repeat method to return a new string repeated 'num' number of times
+  return str.repeat(num);
+}
+
+repeatStringNumTimes("abc", 3);
