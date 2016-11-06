074e6c6bde41f31924ee8d77849fd24a3d326689
michellejanosi <michelle.janosi@gmail.com>
2016-11-06 08:12:42 -0500

    Added explanation for while loop solution

--- basic-algorithm-scripting/factorialize.js
+++ basic-algorithm-scripting/factorialize.js
@@ -21,9 +21,11 @@ factorialize(5);
 // using a 'while' loop
 function factorialize(num) {
   var answer = 1;
-  
+
   while(num > 1) {
+    // while the number is greater than 1, answer will equal answer * num
     answer *= num;
+    // subtract 1 from the number through the loop until we get to 1
     num--;
   }
   return answer;
