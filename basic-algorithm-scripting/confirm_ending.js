2227d43ef17ca4ef3726b34e59f1159730f73cfe
michellejanosi <michelle.janosi@gmail.com>
2016-11-30 11:21:16 -0500

    Added another solution to challenge 🛠

--- basic-algorithm-scripting/confirm_ending.js
+++ basic-algorithm-scripting/confirm_ending.js
@@ -5,8 +5,19 @@ without using the .endsWith() method.
 
 function confirmEnding(str, target) {
   // use the .substr() method to return the characters in the string starting from the string's
-  // end hence using the - in front of target and comparing it to target 
+  // end hence using the - in front of target and comparing it to target
   return str.substr(-target.length) === target;
 }
 
-confirmEnding("Bastian", "n");
+confirmEnding("Bastian", "n"); // returns true
+
+// another solution using the substring() method.
+function confirmEnding(str, target) {
+  // use the .substring() method the substring of str. The .lastIndexOf() method
+  // returns the index number of the last occurance of said target. If the target
+  // is not found, .lastIndexOf() returns -1. All this compared to target returns
+  // true or false.
+  return str.substring(str.lastIndexOf(target)) === target;
+}
+
+confirmEnding("Bastian", "n"); // returns true
