e4d10bb812a25e2d3ba1807837ad6a2d6e031954
michellejanosi <michelle.janosi@gmail.com>
2016-12-03 09:46:11 -0500

    Added solution to challenge 🛠

---
+++ intermediate-algorithm-scripting/diff_two_arrays.js
@@ -0,0 +1,27 @@
+/*
+Compare two arrays and return a new array with any items only found in one of
+the two given arrays, but not both. In other words, return the symmetric difference
+of the two arrays.
+*/
+
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  // loop through array and compare with other array to find the difference using .indexOf()
+  for(var i in arr2) {
+    if(arr1.indexOf(arr2[i]) === -1) {
+      newArr.push(arr2[i]); // push difference to newArr
+    }
+  }
+
+  // loop through array and compare with other array to find the difference using .indexOf()
+  for(var j in arr1) {
+    if(arr2.indexOf(arr1[j]) === -1) {
+      newArr.push(arr1[j]); // push difference to newArr
+    }
+  }
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]); // returns [4]
+diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite",
+"andesite", "grass", "dirt", "dead shrub"]; // returns ["diorite", "pink wool"]
